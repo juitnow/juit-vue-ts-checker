456479421ba27f4f3eb67824a8a953f54c60adb2
Pier Fumagalli <pier@usrz.com>
2021-05-12 01:50:01 +0200

    Removing lots of useless code...

--- src/transpile.ts
+++ src/transpile.ts
@@ -9,26 +9,23 @@ import {
 } from '@vue/compiler-sfc'
 
 import generate from '@babel/generator'
-import { traverse } from '@babel/core'
 import { parse as babelParse } from '@babel/parser'
+
+import assert from 'assert'
 
 import {
   File,
-  Identifier,
-  assertExpression,
   assertIdentifier,
-  file,
   identifier,
   isFunctionDeclaration,
-  program,
   tsTypeAnnotation,
   tsTypeParameterInstantiation,
   tsTypeQuery,
   tsTypeReference,
   traverseFast,
-  variableDeclaration,
-  variableDeclarator,
 } from '@babel/types'
+
+import * as t from '@babel/types'
 
 import { SourceMapConsumer, RawSourceMap } from 'source-map'
 
@@ -59,25 +56,24 @@ export function transpile(fileName: string, source: string): Transpiled | null {
     sourceMap: true,
   })
 
-  if (! vue.script) throw new Error(`No script produced for ${fileName}`)
-  if (! vue.script.map) throw new Error(`No source map produced for ${fileName}`)
+  // Basic checks for what we need
+  assert(vue.script, `No script produced for ${fileName}`)
+  assert(vue.script.map, `No source map produced for ${fileName}`)
+  assert(vue.template, `No template produced for ${fileName}`)
+  assert(vue.template.map, `No template source map produced for ${fileName}`)
 
   // We really want typescript here, otherwise there's nothing we can do...
   if (vue.script.lang !== 'ts') return null
 
-  if (! vue.template) throw new Error(`No template produced for ${fileName}`)
-  if (! vue.template.map) throw new Error(`No template source map produced for ${fileName}`)
-
   // Our script here is the AST representation of our <script>...</script>
-  const script = parseAst(vue.script.content, fileName, vue.script.map)
-  const templateId = crypto.createHash('sha256').update(fileName, 'utf8').digest('hex').substr(0, 8)
+  const id = crypto.createHash('sha256').update(fileName, 'utf8').digest('hex').substr(0, 8)
 
   // Genereate the "render(...)" function from the <template>...</template>
   const template = compileTemplate({
     filename: fileName,
     source: vue.template.content,
     inMap: vue.template.map,
-    id: templateId,
+    id: id,
     scoped: false,
     slotted: false,
     ssrCssVars: vue.cssVars,
@@ -87,127 +83,81 @@ export function transpile(fileName: string, source: string): Transpiled | null {
   })
 
   // We need source maps, our reports are useless otherwise
-  if (! template.map) throw new Error(`No render function source map produced for ${fileName}`)
+  assert(template.map, `No render function source map produced for ${fileName}`)
 
-  return {
-    source: source,
-    script: vue.script.content,
-    scriptSourceMap: vue.script.map,
-    render: template.code,
-    renderSourceMap: template.map,
-  }
-
-  /*
-  // This is tha AST of the Vue "render(...)" function
+  // This is the AST of the Vue "render(...)" function's script
   const render = parseAst(template.code, fileName, template.map)
 
-  // Let's start making some "magic" happen: combine the two ASTs
-  const combined = file(program(
-      [ ...script.program.body, ...render.program.body ],
-      [ ...script.program.directives, ...render.program.directives ],
-  ))
+  // Let's do some AST trickeries...
+  const _id = t.identifier(`__${id}__`)
 
-  // The id here is a variable name we use to assign our component to. This
-  // is used to infer its type, so we can correctly annotate the render
-  // function. So basically normally we get something like:
-  //
-  //     export default defineComponent() { ... }
-  //     export function render(_ctx: any) { ... }
-  //
-  // We want to get those two rewritten as follows:
-  //
-  //     const _default_ = defineComponent() { ... }
-  //     export default _default_
-  //     export function render(_ctx: InstanceType<typeof _default_>)
-  //
-  // So, ultimately, when we pass off this new source to TypeScript, we get
-  // proper type checking between component and template :-) BINGO!
-
-  // The id here is the name of our _default_ variable
-  let id: Identifier | undefined = undefined
+  // Walk the body of the program looking for => export function render(...)
   let annotated = false
+  for (const node of render.program.body) {
+    // Looking for a _named_ export declaration
+    if (! t.isExportNamedDeclaration(node)) continue
 
-  const now = Date.now()
-  traverse(combined, {
-    // export default defineComponent(...)
-    ExportDefaultDeclaration(path) {
-      assertExpression(path.node.declaration)
+    // Make sure this is _really_ "export function render()"
+    const declaration = node.declaration
+    if (! isFunctionDeclaration(declaration)) continue
+    if (declaration.id?.name !== 'render') continue
 
-      // Here we replace
-      //   export default defineComponent(...);
-      // with a simple
-      //   const _default_ = defineComponent(...);
-      //   export default _default_;
-      id ||= path.scope.generateUidIdentifier('_default_')
-      const assignment = variableDeclarator(id, path.node.declaration)
-      const declaration = variableDeclaration('const', [ assignment ])
+    // Make sure the first parameter is an identifier
+    const context = declaration.params[0]
+    assertIdentifier(context) // yup, ASSERT
 
-      // Insert variable assignment _before_ export default
-      path.insertBefore(declaration)
+    // The type of the parameter should be "InstanceType<typeof __id__>", build it!
+    const query = tsTypeQuery(_id) // typeof __id__
+    const instantiation = tsTypeParameterInstantiation([ query ]) // <typeof __id__>
+    const instanceType = identifier('InstanceType') // InstanceType
+    const reference = tsTypeReference(instanceType, instantiation) // InstanceType<typeof __id__>
+    const annotation = tsTypeAnnotation(reference) // :InstanceType<typeof __id__>
 
-      // Replace export default declaration with our id
-      path.node.declaration = id
-    },
+    // Replace "any" with our proper => InstanceType<typeof __id__>
+    context.typeAnnotation = annotation
 
-    // export function render(_ctx: any, ...)
-    ExportNamedDeclaration(path) {
-      const declaration = path.node.declaration
+    // Mark ourselves as "annotated"
+    annotated = true
+    break
+  }
 
-      // Make sure this is _really_ "export function render()"
-      if (isFunctionDeclaration(declaration) && (declaration.id?.name === 'render')) {
-        // Make sure the first parameter is an identifier
-        const context = declaration.params[0]
-        assertIdentifier(context)
+  // Check we annotated the render function
+  assert(annotated, `Unable to annotate render function in ${fileName}`)
 
-        // This is to make sure we use the same name as above
-        id ||= path.scope.generateUidIdentifier('_default_')
+  // Create => import __id__ from './script'
+  const _importDefault = t.importDefaultSpecifier(_id)
+  const _importSource = t.stringLiteral('./script')
+  const _import = t.importDeclaration([ _importDefault ], _importSource)
+  render.program.body.unshift(_import)
 
-        // The type of the parameter should be "InstanceType<typeof _default_>", build it!
-        const query = tsTypeQuery(id) // typeof _default_
-        const instantiation = tsTypeParameterInstantiation([ query ]) // <typeof _default_>
-        const instanceType = identifier('InstanceType') // InstanceType
-        const reference = tsTypeReference(instanceType, instantiation) // InstanceType<typeof _default_>
-        const annotation = tsTypeAnnotation(reference) // :InstanceType<typeof _default_>
-
-        // Replace "any" with our proper "InstanceType<typeof _default_>"
-        context.typeAnnotation = annotation
-
-        // Mark ourselves as "annotated"
-        annotated = true
-      }
-    },
-  })
-  console.log('TRAVERSE IN', Date.now() - now)
-
-  // If we can't annotate, fail... It'd be pointless anyway
-  if (! annotated) throw new Error(`Unable to annontate render function for ${fileName}`)
-
-  // Now we generate the full combined TypeScript source from our annotated
-  // AST, and remember our source map, too!
-  const generated = generate(combined, {
+  // Now we re-generate the full source back from our AST again...
+  const generated = generate(render, {
     sourceMaps: true,
     filename: fileName,
     sourceFileName: fileName,
-    // minified: true,
     comments: false,
   }, source)
 
   // We need the source map, our reports would be pointless without
-  if (! generated.map) throw new Error(`No source map generated transpiling ${fileName}`)
+  assert(generated.map, `No source map generated transpiling ${fileName}`)
 
-  const generatedMap: RawSourceMap = {
-    version: generated.map.version === undefined ? '3' : generated.map.version.toString(),
-    sources: generated.map.sources,
-    names: generated.map.names,
-    sourceRoot: generated.map.sourceRoot,
-    sourcesContent: generated.map.sourcesContent,
-    mappings: generated.map.mappings,
-    file: generated.map.file,
+  // Well, we're actually done here!
+  return {
+    source: source,
+    script: vue.script.content,
+    scriptSourceMap: vue.script.map,
+    render: generated.code,
+    renderSourceMap: {
+      // Silly... Babel source maps and SourceMap source maps are incompatble
+      version: generated.map.version === undefined ? '3' : generated.map.version.toString(),
+      sources: generated.map.sources,
+      names: generated.map.names,
+      sourceRoot: generated.map.sourceRoot,
+      sourcesContent: generated.map.sourcesContent,
+      mappings: generated.map.mappings,
+      file: generated.map.file,
+    },
   }
-
-  // Done.. Return our content and sourceMap
-  return { content: generated.code, template: source, sourceMap: generatedMap }
-  */
 }
 
 // Parse some typescript code into an AST, update all of the AST locations
@@ -226,9 +176,8 @@ function parseAst(code: string, fileName: string, sourceMap: RawSourceMap): File
   // I found no other way to get all _position_ other than traversing the
   // entire AST and go through each _location_, and map each _position_.
   //
-  // But we need to keep state, as we don't want to translate the original
+  // We also need to keep state, as we don't want to translate the original
   // position twice (and therefore screw up the entire tree)
-
   const sourceMapConsumer = new SourceMapConsumer(sourceMap)
 
   function translate(position: { line: number, column: number, __marked?: boolean }): boolean {
