c72322315cfbfbea8be0ee7018cbb5c07c79466a
Pier Fumagalli <pier@usrz.com>
2021-05-16 17:40:50 +0200

    Initial reports checking and exit codes

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,17 @@ import { reporter } from './reporter'
 import { createChecker } from './checker'
 
 import {
-  formatDiagnosticsWithColorAndContext,
   readConfigFile,
   sys,
 } from 'typescript'
+import { diagnosticsReports } from './reports'
 
 /** Read a `tsconfig` file to extract the `include` and `exclude` */
 function readTsConfig(): { exclude: string[], include: string[] } {
   if (sys.fileExists('tsconfig.json')) {
     const config = readConfigFile('tsconfig.json', sys.readFile)
     if (config.error) {
-      formatDiagnosticsWithColorAndContext([ config.error ], {
-        getCurrentDirectory: sys.getCurrentDirectory,
-        getCanonicalFileName: (file: string) => file,
-        getNewLine: () => sys.newLine,
-      })
+      reporter(diagnosticsReports([ config.error ]))
       process.exit(1)
     }
 
@@ -31,6 +27,15 @@ function readTsConfig(): { exclude: string[], include: string[] } {
 // Mark our time
 const now = Date.now()
 
+// Create our checker, and get our initial reports
+const checker = createChecker()
+const reports = checker.init()
+
+if (reports.hasErrors) {
+  reporter(reports)
+  process.exit(1)
+}
+
 // Read our `tsconfig` to get includes and excludes
 const { exclude, include } = readTsConfig()
 
@@ -42,8 +47,10 @@ const files = sys.readDirectory(
     include,
 )
 
-// Check and report!
-reporter(createChecker().check(...files))
+// Run only if we don't have errors in "tsconfig"
+reports.push(...checker.check(...files))
+reporter(reports)
 
 // eslint-disable-next-line no-console
 console.log('\nChecked', files.length, 'files in', (Date.now() - now) / 1000, 'sec')
+if (reports.hasErrors) process.exit(1)
