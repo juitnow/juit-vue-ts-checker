51b392537b779c441c772599d22b03224404199b
Pier Fumagalli <pier@usrz.com>
2021-05-16 16:29:59 +0200

    Export all

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ export * from './reporter'
 export * from './reports'
 export * from './transpile'
 
+export * from './lib/cache'
+export * from './lib/colors'
 export * from './lib/files'
 export * from './lib/logger'
 export * from './lib/pseudo'
