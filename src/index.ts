b31d13723812446c8e87c245448f495aeb2417bf
Pier Fumagalli <pier@usrz.com>
2021-05-16 22:41:29 +0200

    Clean up exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 export * from './async/index'
-
 export * from './core/checker'
-export * from './core/compiler'
-export * from './core/reporter'
-export * from './core/reports'
-export * from './core/transpile'
-
-export * from './lib/cache'
-export * from './lib/colors'
-export * from './lib/files'
-export * from './lib/logger'
-export * from './lib/pseudo'
+export * from './webpack'
