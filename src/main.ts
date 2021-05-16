1f509dd1341a22eef84151a56b20d1ba0673d4bb
Pier Fumagalli <pier@usrz.com>
2021-05-16 02:01:38 +0200

    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,106 +1,64 @@
+/* eslint-disable no-console */
+
 import { promises as fs } from 'fs'
-import { Checker } from './checker'
-import { K, R, X, Y, G, C, M, k, r, y, w, c, m } from './lib/colors'
-import { OS_EOL, resolve } from './lib/files'
+import { resolve } from './lib/files'
+import { reporter } from './reporter'
 import { Reports } from './reports'
+import { createAsyncChecker } from './async'
 
-function report(reports: Reports): void {
-  const write = process.stdout.write.bind(process.stdout)
+async function main(): Promise<void> {
+  const promises: Promise<Reports>[] = []
+  const checker = await createAsyncChecker()
+  const reports = await checker.init('tsconfig.json')
 
-  write(`Generated ${y(reports.length)} reports`)
-  if (reports.hasErrors || reports.hasWarnings) write(k(' ('))
-  if (reports.hasErrors) write(r('has errors'))
-  if (reports.hasErrors && reports.hasWarnings) write(k(', '))
-  if (reports.hasWarnings) write(y('has warnings'))
-  if (reports.hasErrors || reports.hasWarnings) write(k(')'))
-  write('\n')
+  promises.push(checker.check('src/main.ts'))
+  const paths: string[] = []
 
-  for (const report of reports) {
-    write('\n')
-    write(w(`TS${report.code.toString().padStart(4, '0')} `))
+  async function check(path: string, type?: 'file' | 'dir'): Promise<void> {
+    const resolved = resolve(path)
 
-    switch (report.severity) {
-      case 'error': write(`${K}[${R}ERROR${K}]${X} `); break
-      case 'warning': write(`${K}[${Y}WARNING${K}]${X} `); break
-      case 'message': write(`${K}[${G}MESSAGE${K}]${X} `); break
-      case 'suggestion': write(`${K}[${C}SUGGESTION${K}]${X} `); break
-      default: write(`${K}[${M}UNKNOWN${K}]${X} `); break
+    if (! type) {
+      const stat = await fs.stat(resolved)
+      if (stat.isDirectory()) type = 'dir'
+      else if (stat.isFile()) type = 'file'
+      else return
     }
 
-    const lines = report.message.split(OS_EOL)
-    const stack = lines.splice(1)
-
-    write(lines[0])
-    for (const line of stack) {
-      write(k('\n | '))
-      write(line)
-    }
-    write('\n')
-
-    if (report.fileName) {
-      write(k(' | at '))
-      write(c(report.fileName))
-      if (report.location) {
-        const { line, column, context, contextLength } = report.location
-        write(k(' line '))
-        write(y(line))
-        if (column > 0) {
-          write(k(' col '))
-          write(y(column))
-        }
-        if (context) {
-          write(k('\n |\n | '))
-          if ((column > 0) && (contextLength > 0)) {
-            write(context.substr(0, column))
-            write(w(context.substr(column, contextLength)))
-            write(context.substr(column + contextLength))
-
-            write(k('\n | '))
-            write(' '.repeat(column))
-            write(m('~'.repeat(contextLength)))
-          } else {
-            write(context)
-          }
-        }
+    if (type === 'dir') {
+      const entries = await fs.readdir(resolved, { withFileTypes: true })
+      for (const entry of entries) {
+        const name = resolve(resolved, entry.name)
+        if (entry.isDirectory()) await check(name, 'dir')
+        else if (entry.isFile()) await check(name, 'file')
       }
-      write('\n')
+    } else if ((type === 'file') && (resolved.match(/\.(ts|vue)$/))) {
+      // promises.push(checker.check(resolved))
+      paths.push(resolved)
     }
   }
-}
 
-const checker = new Checker('tsconfig.json')
-let reports = checker.check('src/main.ts')
+  await check('src')
+  await checker.check(...paths)
+  console.log('Done scan:', Date.now() - now, 'ms')
 
-async function check(path: string, type?: 'file' | 'dir'): Promise<void> {
-  const resolved = resolve(path)
+  // await check('src')
+  reports.push(...await checker.check(...paths))
+  console.log('Done scan 2:', Date.now() - now, 'ms')
 
-  if (! type) {
-    const stat = await fs.stat(resolved)
-    if (stat.isDirectory()) type = 'dir'
-    else if (stat.isFile()) type = 'file'
-    else return
-  }
+  // const results = await Promise.all(promises)
 
-  if (type === 'dir') {
-    const entries = await fs.readdir(resolved, { withFileTypes: true })
-    for (const entry of entries) {
-      const name = resolve(resolved, entry.name)
-      if (entry.isDirectory()) await check(name, 'dir')
-      else if (entry.isFile()) await check(name, 'file')
-    }
-  } else if ((type === 'file') && (resolved.match(/\.(ts|vue)$/))) {
-    const fileReports = checker.check(resolved)
-    if (reports) reports.push(...fileReports)
-    else reports = fileReports
-  }
+  // results.forEach((result) => {
+  //   reports.push(...result)
+  // })
+
+  reporter(reports)
+  await checker.destroy()
 }
 
 const now = Date.now()
-check('src')
+
+main()
     .then(() => {
-      if (reports) report(reports)
       console.log('Full run:', Date.now() - now, 'ms')
     })
-    .catch((error) => console.log(error))
-
-// report(checker.check('src/components/faq.vue'))
+    .catch((error) => console.error(error))
